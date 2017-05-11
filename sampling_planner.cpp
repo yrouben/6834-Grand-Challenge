ee34a00e492812558ab4a24585142c09b57873c1
yrouben <yousif@mit.edu>
2017-05-11 15:44:24 -0400

    cpp code skeleton

---
+++ sampling_planner.cpp
@@ -0,0 +1,43 @@
+#include <pluginlin/class_list_macros.h>
+#include "global_planner.h"
+
+PLUGINLIB_EXPORT_CLASS(sampling_planner::SamplingPlanner, nav_core::BaseGlobalPlanner)
+
+using namespace std;
+
+namespace sampling_planner {
+
+SamplingPlanner::SamplingPlanner (){
+
+}
+
+SamplingPlanner::SamplingPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
+    initialize(name, costmap_ros);
+}
+
+
+void SamplingPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
+
+}
+
+bool SamplingPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped& plan){
+    // This is a dummer makeplan code. we just need to call our path planner here.
+    plan.push_back(start);
+    for (int i=0; i<20; i++){
+        geometry_msgs::PoseStamped new_goal = goal;
+        tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+        new_goal.pose.position.x = -2.5+(0.05*i);
+        new_goal.pose.position.y = -3.5+(0.05*i);
+
+        new_goal.pose.orientation.x = goal_quat.x();
+        new_goal.pose.orientation.y = goal_quat.y();
+        new_goal.pose.orientation.z = goal_quat.z();
+        new_goal.pose.orientation.w = goal_quat.w();
+
+    plan.push_back(new_goal);
+    }
+
+    plan.push_back(goal);
+    return true;
+}
+};
